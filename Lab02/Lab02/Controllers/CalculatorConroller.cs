093953a1380b79feecb4f81945b9cf0f3edaf3cd
HIENLTH <hyhien@gmail.com>
2026-06-17 11:32:18 +0700

    Demo Lab02 - 01 - Calculator

--- Lab02/Lab02/Controllers/CalculatorConroller.cs
+++ Lab02/Lab02/Controllers/CalculatorConroller.cs
@@ -2,11 +2,38 @@
 
 namespace Lab02.Controllers
 {
-    public class CalculatorConroller : Controller
+    public class CalculatorController : Controller
     {
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Calculate(double SoHang01, double SoHang02, string ToanTu)
+        {
+            double result = 0;
+            switch (ToanTu)
+            {
+                case "+": result = SoHang01 + SoHang02; break;
+                case "-": result = SoHang01 - SoHang02; break;
+                case "*": result = SoHang01 * SoHang02; break;
+                case "/":
+                    if (SoHang02 == 0)
+                    {
+                        // Handle division by zero
+                        ViewData["Error"] = "Cannot divide by zero";
+                    }
+                    else
+                    {
+                        result = SoHang01 / SoHang02;
+                    }
+                    break;
+            }
+            ViewData["Result"] = result;
+            ViewBag.SoHang01 = SoHang01;
+            ViewBag.SoHang02 = SoHang02;
+            ViewBag.ToanTu = ToanTu;
+            return View("Index");
         }
     }
 }
