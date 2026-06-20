7a702bc89f93244b589d9dbd9f96c7b964894b29
HIENLTH <hyhien@gmail.com>
2026-06-20 11:57:50 +0700

    Upload file

---
+++ Buoi04/Buoi04/Controllers/FileUploadController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static System.Net.WebRequestMethods;
+
+namespace Buoi04.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        public IActionResult UploadFile(IFormFile myfile)
+        {
+            if (myfile != null)
+            {
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "data", myfile.FileName);
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    myfile.CopyTo(stream);
+                }
+                TempData["Message"] = "File uploaded successfully!";
+            }
+            else
+            {
+                TempData["Message"] = "No file selected.";
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
