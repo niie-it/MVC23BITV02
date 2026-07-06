19cadbe93293d9f93ff251fd83fb7d25fa9a3fb9
HIENLTH <hyhien@gmail.com>
2026-07-06 09:21:57 +0700

    EF Core - DB First

---
+++ Buoi10_DBFirst/Buoi10_DBFirst/Controllers/HangHoasController.cs
@@ -0,0 +1,30 @@
+﻿using Buoi10_DBFirst.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Buoi10_DBFirst.Controllers
+{
+    public class HangHoasController : Controller
+    {
+        private readonly MyeStoreContext _context;
+
+        public HangHoasController(MyeStoreContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var dsHangHoa = _context.HangHoas
+                .Select(h => new Models.HangHoaVM
+                {
+                    MaHh = h.MaHh,
+                    TenHh = h.TenHh,
+                    Hinh = h.Hinh,
+                    DonGia = h.DonGia ?? 0,
+                    TenLoai = h.MaLoaiNavigation.TenLoai,
+                    NhaCungCap = h.MaNccNavigation.TenCongTy
+                }) .ToList();
+            return View(dsHangHoa);
+        }
+    }
+}
---
+++ Buoi10_DBFirst/Buoi10_DBFirst/Models/HangHoaVM.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Buoi10_DBFirst.Models
+{
+    public class HangHoaVM
+    {
+        [Key]
+        public int MaHh { get; set; }
+        public string TenHh { get; set; } = null!;
+        public string Hinh { get; set; } = null!;
+        public double DonGia { get; set; }
+        public string TenLoai { get; set; }
+        public string NhaCungCap { get; set; }
+    }
+}
