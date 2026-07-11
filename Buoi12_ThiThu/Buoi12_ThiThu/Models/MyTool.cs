29c63ee488b3a4e443b6de3a185d12a5bf60fd0b
HIENLTH <hyhien@gmail.com>
2026-07-11 15:25:25 +0700

    CRUD

---
+++ Buoi12_ThiThu/Buoi12_ThiThu/Models/MyTool.cs
@@ -0,0 +1,25 @@
+﻿namespace Buoi12_ThiThu.Models
+{
+    public class MyTool
+    {
+        public static async Task<string> SaveFile(IFormFile file, string folderPath)
+        {
+            try
+            {
+                // Generate a unique file name for the uploaded image
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images", folderPath, fileName);
+                // Save the uploaded image to the server
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+                return fileName;
+            }
+            catch
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
